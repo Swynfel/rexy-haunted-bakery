3a67bfb9c37ce8af2cd82e9bc91fe80532c470c0
Quentin Gendre <swynfel@gmail.com>
2020-10-14 20:07:57 +0900

    Add jump with coyote time and controllable height

--- Scripts/Rexy.cs
+++ Scripts/Rexy.cs
@@ -3,7 +3,10 @@ using Godot;
 
 public class Rexy : KinematicBody2D {
 
-    [Export] public float VerticalAcceleration = 10f;
+    [Export] public float VerticalAcceleration = 1000f;
+    [Export] public float JumpVelocity = 300f;
+    [Export] public float HorizontalSpeed = 100f;
+    [Export] public float CoyoteTime = 0.1f;
     public AnimatedSprite Anim;
     public Vector2 Velocity = new Vector2();
     public override void _Ready() {
@@ -11,8 +14,41 @@ public class Rexy : KinematicBody2D {
         Anim.Playing = true;
     }
 
+    public bool IsOnGround => !Jumped && timeSinceLeftFloor <= CoyoteTime;
+    public bool Jumped = false;
+    private float timeSinceLeftFloor;
+
+    public int GetXDirection() {
+        int x = 0;
+        if (Input.IsActionPressed("ui_left")) {
+            x--;
+        }
+        if (Input.IsActionPressed("ui_right")) {
+            x++;
+        }
+        return x;
+    }
+    private void RefreshPhysicsState(float delta) {
+        if (IsOnFloor()) {
+            timeSinceLeftFloor = 0f;
+        } else {
+            timeSinceLeftFloor += delta;
+        }
+    }
     public override void _PhysicsProcess(float delta) {
-        Velocity += delta * VerticalAcceleration * Vector2.Down;
-        Velocity = MoveAndSlide(Velocity);
+        RefreshPhysicsState(delta);
+        Velocity.x = HorizontalSpeed * GetXDirection();
+        if (Input.IsActionJustPressed("jump") && IsOnGround) {
+            Velocity.y = -JumpVelocity;
+            Jumped = true;
+        } else if (Input.IsActionJustReleased("jump")) {
+            if (Velocity.y < 0) {
+                Velocity.y /= 2;
+            }
+            Jumped = false;
+        } else {
+            Velocity += delta * VerticalAcceleration * Vector2.Down;
+        }
+        Velocity = MoveAndSlide(Velocity, upDirection: Vector2.Up);
     }
 }
