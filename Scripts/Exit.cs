7a94594a2fe1b3784c10e54a0b78e59dd5c2e755
Quentin Gendre <swynfel@gmail.com>
2020-10-16 16:33:10 +0900

    Add instructions and swinging exit arrow

--- Scripts/Exit.cs
+++ Scripts/Exit.cs
@@ -4,6 +4,7 @@ using Godot;
 public class Exit : Area2D {
     public override void _Ready() {
         Connect("body_entered", this, nameof(BodyEntered));
+        GetNode<AnimationPlayer>("Anim").Play("base");
     }
 
     public void BodyEntered(PhysicsBody2D body) {
