8af52eb231aa1cb2dfb89a431ead33729f122d51
Quentin Gendre <swynfel@gmail.com>
2020-10-18 17:51:42 +0900

    Fix not able to go back to menu

--- Scripts/VisualEffects/AutoAnimOnlyAtStart.cs
+++ Scripts/VisualEffects/AutoAnimOnlyAtStart.cs
@@ -5,10 +5,15 @@ public class AutoAnimOnlyAtStart : Node {
     private static bool start = true;
     [Export] NodePath Path;
     [Export] string Animation;
+    [Export] string OtherAnimation;
     public override void _Ready() {
         if (start) {
             start = false;
             GetNode<AnimationPlayer>(Path).Play(Animation);
+        } else {
+            if (OtherAnimation != "") {
+                GetNode<AnimationPlayer>(Path).Play(OtherAnimation);
+            }
         }
     }
 }
