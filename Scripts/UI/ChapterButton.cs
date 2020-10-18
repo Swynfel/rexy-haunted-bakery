a1eb5d57e2061ade6c4de7eea78df7c13595439d
Quentin Gendre <swynfel@gmail.com>
2020-10-18 19:23:32 +0900

    Add palette to menu button; Tweak tutorial levels

--- Scripts/UI/ChapterButton.cs
+++ Scripts/UI/ChapterButton.cs
@@ -4,6 +4,8 @@ using Utils;
 
 public class ChapterButton : GrabFocusButton {
     [Export] ChapterId chapter;
+    [Export] Palette palette;
+    [Export] bool useWallColor;
 
     public override void _Ready() {
         base._Ready();
@@ -12,6 +14,18 @@ public class ChapterButton : GrabFocusButton {
         if (Global.Chapter == chapter) {
             GrabFocus();
         }
+        StyleBoxFlat normal = (StyleBoxFlat) Get("custom_styles/normal");
+        StyleBoxFlat focused = (StyleBoxFlat) Get("custom_styles/focus");
+        Color bg = useWallColor ? palette.wallInner : palette.backgroundInner;
+        Color border = useWallColor ? palette.wallOuter : palette.backgroundOuter;
+        Color textA = useWallColor ? palette.backgroundInner : palette.wallInner;
+        Color textB = useWallColor ? palette.backgroundOuter : palette.wallOuter;
+        normal.BgColor = bg;
+        normal.BorderColor = border;
+        focused.BgColor = bg;
+        focused.BorderColor = border;
+        GetNode("Name").Set("custom_colors/font_color", textA);
+        GetNode("Chapter").Set("custom_colors/font_color", textB);
     }
 
     protected override void ButtonPressed() {
